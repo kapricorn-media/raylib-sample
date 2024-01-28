738165e53003cd5b312e1246a9e3c06090543e4f
Jose Rico <jmrico01@gmail.com>
2024-01-27 23:48:52 -0500

    add example dep and switch to url dep

--- build.zig
+++ build.zig
@@ -7,32 +7,32 @@ pub fn build(b: *std.Build) !void
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Set up the raylib-enabled compile step.
     const raylibZig = b.dependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
     });
-
     const compile = try raylibBuild.setup(b, raylibZig, .{
         .name = "raylib-sample",
         .src = "src/main.zig",
         .target = target,
         .optimize = optimize,
+        .createRunStep = true,
     });
     if (target.result.os.tag == .windows) {
         compile.subsystem = .Windows;
     }
 
-    // clientExe.root_module.addImport("raygui", rayguiZig.module("raygui_zig"));
+    // You can add other 3rd party libraries to the compile step.
+    const depModule = b.createModule(.{
+        .root_source_file = .{.path = "src/dep.zig"},
+        .target = target,
+        .optimize = optimize,
+    });
+    compile.root_module.addImport("dep", depModule);
+
+    // You can choose to add the compile step to the default install, or do something else.
     b.installArtifact(compile);
-
-    // const runCmd = b.addRunArtifact(compile);
-    // runCmd.step.dependOn(b.getInstallStep());
-    // if (b.args) |args| {
-    //     runCmd.addArgs(args);
-    // }
-
-    // const runStep = b.step("run", "Run");
-    // runStep.dependOn(&runCmd.step);
 
     const testStep = b.step("test", "Test");
     _ = testStep;
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const rl = @import("raylib");
-const rg = @import("raygui");
+
+const dep = @import("dep"); // example external dependency
 
 pub fn main() void {
     rl.SetConfigFlags(rl.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
@@ -11,9 +12,9 @@ pub fn main() void {
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
         defer rl.EndDrawing();
-        
+
         rl.ClearBackground(rl.BLACK);
-        rl.DrawFPS(10, 10);
+        rl.DrawFPS(10, dep.add(5, 5));
 
         rl.DrawText("hello world!", 100, 100, 20, rl.YELLOW);
     }
